80d3e86a8bdf57aaf30b3cb27db168e0e5d5ee40
Julia Holadava <juliaholadava@gmail.com>
2024-02-20 11:54:06 +0100

    feat: implement HTTP server to serve static frontend files

--- src/http_server/index.js
+++ src/http_server/index.js
@@ -3,15 +3,29 @@ import * as path from 'path';
 import * as http from 'http';
 
 export const httpServer = http.createServer(function (req, res) {
-    const __dirname = path.resolve(path.dirname(''));
-    const file_path = __dirname + (req.url === '/' ? '/front/index.html' : '/front' + req.url);
-    fs.readFile(file_path, function (err, data) {
+    const __dirname = path.resolve();
+    const filePath = path.join(__dirname, 'front', req.url === '/' ? 'index.html' : req.url);
+
+    let contentType = 'text/html';
+    switch (path.extname(filePath)) {
+        case '.js':
+            contentType = 'application/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+    }
+
+    fs.readFile(filePath, function (err, data) {
         if (err) {
-            res.writeHead(404);
-            res.end(JSON.stringify(err));
+            res.writeHead(404, {'Content-Type': 'application/json'});
+            res.end(JSON.stringify({error: 'File not found'}));
             return;
         }
-        res.writeHead(200);
+        res.writeHead(200, {'Content-Type': contentType});
         res.end(data);
     });
 });
